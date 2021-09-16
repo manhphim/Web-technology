d92b5cf960354f54200022b4eb3bf7ad87987f13
Melissa Neamt <498265@student.saxion.nl>
2021-09-16 17:23:48 +0200

    add filters to auctions get requests

--- backend/src/routes/auctions.js
+++ backend/src/routes/auctions.js
@@ -5,13 +5,36 @@ const { StatusCodes } = require('http-status-codes');
 const auctions = require('../data/auctions');
 const isLoggedIn = require('../middleware/is-logged-in');
 
-
 router.get('', (req, res) => {
-    res.send(auctions);
+    const filters = req.query;
+
+    if (Object.keys(filters).length === 0) {
+        res.send(auctions);
+    } else {
+        const filteredAuctions = auctions.filter(auction => {
+            let isValid = true;
+            for (let key in filters) {
+                isValid = isValid && auction[key] === filters[key];
+            }
+            return isValid;
+        });
+        res.send(filteredAuctions);
+    }
 });
 
 router.get('/:id', (req, res) => {
-    res.send(`Auction with id ${req.params.id} in progress...`);
+    const id = req.params.id;
+    const auction = auctions.find((auction) => {
+        return auction.id == id;
+    })
+
+    if (auction) {
+        res.send(auction);
+    } else {
+        res
+            .status(StatusCodes.NOT_FOUND)
+            .send(`Auction with id ${id} not found`);
+    }
 });
 
 router.post('', isLoggedIn, (req, res) => {
