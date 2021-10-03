ff43d8fbe090372afd319e2b7c009c4ac81db4cf
Melissa Neamt <498265@student.saxion.nl>
2021-10-03 19:06:33 +0200

    add new auction objects to data

--- backend/src/data/auctions.js
+++ backend/src/data/auctions.js
@@ -2,43 +2,92 @@ const auctions = [
     {
         id: 1,
         item : 'Fair Rosamond',
+        image: 'images/fair-rosamund.jpg',
         category : 'Arts',
         isClosed : 'false',
         startingTime : 'December 12, 2021 8:00 PM',
         duration : '',
         details : 'Painting by Kate Muriel Mason',
-        startingPrice : '30000 GBP',
+        startingPrice : '30000 GBP'
     },
     {
         id: 2,
-        item : 'Fair Melissa',
-        category : 'Arts',
-        isClosed : 'false',
-        startingTime : 'December 12, 2021 8:00 PM',
-        duration : '',
-        details : 'Painting by Kate Muriel Mason',
-        startingPrice : '30000 GBP',
-    },
-    {
-        id: 3,
-        item : 'Antique sideboard',
-        category : 'Furniture',
+        item : 'Chromatic Harmonica',
+        image: 'images/chromatic-harmonica.jpg',
+        category : 'Musical instruments',
         isClosed : 'true',
         startingTime : 'December 31, 2021 6:00 PM',
         duration : '',
-        details : 'This is an antique sideboard, an English, oak, Arts & Crafts cabinet in the Liberty taste dating to circa 1900.',
-        startingPrice : '1695 GBP',
+        details : 'Huang Professional 1248, Chromatic Harmonica.',
+        startingPrice : '1695 GBP'
     },
     {
-        id: 4,
-        item : 'Pottery vase',
+        id: 3,
+        item : 'Pottery Vase',
+        image: 'images/pottery-vase.jpg',
         category : 'Crafts',
         isClosed : 'false',
         startingTime : 'October 8, 2021 3:00 PM',
         duration : '',
-        details : 'Pottery vase by George Cartlidge',
-        startingPrice : '595 GBP',
+        details : 'Pottery vase by George Cartlidge.',
+        startingPrice : '595 GBP'
     },
+    {
+        id: 4,
+        item : 'Guillaume Berleur Pistol',
+        image: 'images/pistol.jpg',
+        category : 'Guns',
+        isClosed : 'false',
+        startingTime : 'December 31, 2021 8:00 PM',
+        duration : '',
+        details : 'Double barrel and trigger side by side flintlock pistol by the Belgian gunsmith Guillaume Berleur.',
+        startingPrice : '125 GBP'
+    },
+    {
+        id: 5,
+        item : 'Cactus Bloom',
+        image: 'images/cactus-bloom.jpg',
+        category : 'Arts',
+        isClosed : 'false',
+        startingTime : 'November 12, 2021 4:00 PM',
+        duration : '',
+        details : 'Painting by Edmund Paul',
+        startingPrice : '6100 GBP'
+    },
+    {
+        id: 6,
+        item : 'Musical Putti',
+        image: 'images/musical-putti.jpg',
+        category : 'Jewelry',
+        isClosed : 'false',
+        startingTime : 'November 20, 2021 6:00 PM',
+        duration : '',
+        details : 'Musical Putti and Shell Silver Plated Salt',
+        startingPrice : '4000 GBP'
+    },
+    {
+        id: 7,
+        item : '20th Century Banjo',
+        image: 'images/banjo.jpg',
+        category : 'Musical instruments',
+        isClosed : 'false',
+        startingTime : 'November 30, 2021 6:00 PM',
+        duration : '',
+        details : 'Early to mid 20thC Banjo.',
+        startingPrice : '2000 GBP'
+    },
+
+    {
+        id: 8,
+        item : '19th Century German Violin',
+        image: 'images/german-violin.jpg',
+        category : 'Musical instruments',
+        isClosed : 'false',
+        startingTime : 'October 8, 2021 2:00 PM',
+        duration : '',
+        details : '19thC German violin, of stained maple construction with carved lion\'s head scroll, the tailpiece with inlaid floral decoration.',
+        startingPrice : '500 GBP'
+    }
 ]
 
 module.exports = auctions;
