77ee516d863b28c10013e3988b6b0abe38ffa36c
Melissa Neamt <498265@student.saxion.nl>
2021-10-03 20:32:43 +0200

    fix logo

--- frontend/src/components/Home.svelte
+++ frontend/src/components/Home.svelte
@@ -10,16 +10,16 @@
 <nav>
     <div class="nav-logo">
         <img src="{logo.path}" alt="{logo.id}">
-
     </div>
-    <div class="nav-links"></div>
+    <div class="nav-links">
+        <a href="/users">Users</a>
+    </div>
 </nav>
 <main>
     <div class="auctions">
         <Auction/>
     </div>
 </main>
-
 
 <style>
     nav {
@@ -31,9 +31,29 @@
         border: 1px solid #ddd;
     }
 
+    .nav-links {
+        margin-right: 100px;
+        display: flex;
+        justify-content: space-around;
+    }
+
+    a {
+        position: relative;
+        padding: 1.2rem 1.5rem;
+        margin: 1rem 1rem;
+        text-decoration: none;
+        color: #477592;
+        font-size: 1.6rem;
+        font-family: Andale Mono, monospace;
+        text-align: center;
+    }
+
+    .nav-logo {
+        margin-left: 20px;
+    }
     img {
-        max-width: 300px;
-        max-height: 200px;
+        width: 100%;
+        height: 100%;
     }
 
     main {
