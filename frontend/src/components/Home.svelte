e26b20f7ce355282f54e126fe2137beff2950e5c
Melissa Neamt <498265@student.saxion.nl>
2021-10-03 19:07:45 +0200

    add Auction component to Home page

--- frontend/src/components/Home.svelte
+++ frontend/src/components/Home.svelte
@@ -1,5 +1,50 @@
 <script>
+    import Auction from "./Auction.svelte";
+    let logo = {
+        id: 'logo',
+        path: 'images/logo2.png'
+    }
 
 </script>
 
-<h1>success baby!</h1>
+<nav>
+    <div class="nav-logo">
+        <img src="{logo.path}" alt="{logo.id}">
+
+    </div>
+    <div class="nav-links"></div>
+</nav>
+<main>
+    <div class="auctions">
+        <Auction/>
+    </div>
+</main>
+
+
+<style>
+    nav {
+        position: relative;
+        display: flex;
+        justify-content: space-between;
+        height: 100px;
+        background-color: #fff;
+        border: 1px solid #ddd;
+    }
+
+    img {
+        max-width: 300px;
+        max-height: 200px;
+    }
+
+    main {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+    .auctions {
+        display: block;
+        margin: auto 50px auto 50px;
+        padding: 50px;
+    }
+
+</style>
