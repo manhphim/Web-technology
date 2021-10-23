1cfc1448aa90d6928660c5332dcc77e085865427
Melissa Neamt <498265@student.saxion.nl>
2021-10-23 21:24:19 +0200

    add AuctionBids component

---
+++ frontend/src/components/AuctionBids.svelte
@@ -0,0 +1,36 @@
+<script>
+    export let bids = [];
+</script>
+
+<div class="table-container">
+    {#if bids.length !== 1} <!-- the first "bid" is the start price -->
+        <table>
+            <thead>
+                <tr>
+                    <th>User</th>
+                    <th>Time</th>
+                    <th>Price</th>
+                </tr>
+            </thead>
+            <tbody>
+                {#each bids as bid}
+                    <tr>
+                        <td>{bid.username}</td>
+                        <td>{bid.time}</td>
+                        <td>${bid.price}</td>
+                    </tr>
+                {/each}
+            </tbody>
+        </table>
+    {/if}
+</div>
+<style>
+    table {
+        text-align: center;
+        table-layout: fixed;
+        width: 100%;
+        height: auto;
+        border-spacing: 0;
+        border-collapse: separate;
+    }
+</style>
