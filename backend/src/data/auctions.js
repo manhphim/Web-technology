09fd21499a217135f175d1c6913fc05acdb77bf6
Melissa Neamt <498265@student.saxion.nl>
2021-10-23 19:25:53 +0200

    add new auctions to static data

--- backend/src/data/auctions.js
+++ backend/src/data/auctions.js
@@ -87,6 +87,102 @@ const auctions = [
         endTime : 'December 30, 2021 2:00 PM',
         details : '19thC German violin, of stained maple construction with carved lion\'s head scroll, the tailpiece with inlaid floral decoration.',
         startPrice : '500$'
+    },
+
+    {
+        id: 9,
+        item : 'Pottery Face Jug',
+        image: 'http://localhost:3000/images/pottery-face-jug.jpg',
+        category : 'Crafts',
+        status : 'Closed',
+        startTime : 'October 30, 2021 3:00 PM',
+        endTime : 'December 10, 2021 2:00 PM',
+        details : 'Late 20th Century; Original, 11" height, Overall excellent condition. Appears to be complete and all original.',
+        startPrice : '120$'
+    },
+
+    {
+        id: 10,
+        item : 'Charter Arms Revolver',
+        image: 'http://localhost:3000/images/revolver.jpg',
+        category : 'Guns',
+        status : 'Closed',
+        startTime : 'November 14, 2021 7:00 PM',
+        endTime : 'December 10, 2021 2:00 PM',
+        details : 'Traditional DA 5 shot sm. frame revolver',
+        startPrice : '200$'
+    },
+
+    {
+        id: 11,
+        item : 'Gold Eagle Diamond Necklace',
+        image: 'http://localhost:3000/images/gold-necklace.jpg',
+        category : 'Jewelry',
+        status : 'Closed',
+        startTime : 'December 12, 2021 5:00 PM',
+        endTime : 'December 20, 2021 10:00 PM',
+        details : 'Gold Eagle pendant necklace',
+        startPrice : '300$'
+    },
+
+    {
+        id: 12,
+        item : 'Copper Hanging Parrot Sculpture',
+        image: 'http://localhost:3000/images/parrot-sculpture.jpg',
+        category : 'Arts',
+        status : 'Closed',
+        startTime : 'December 16, 2021 1:00 PM',
+        endTime : 'December 21, 2021 3:00 PM',
+        details : 'Mid-Century Modern Brass & Copper Hanging Parrot Sculpture.',
+        startPrice : '185$'
+    },
+
+    {
+        id: 13,
+        item : 'Crystal Ducks & Turtle Figurines',
+        image: 'http://localhost:3000/images/crystal-figurines.jpg',
+        category : 'Arts',
+        status : 'Open',
+        startTime : 'October 16, 2021 12:00 PM',
+        endTime : 'November 12, 2021 3:00 PM',
+        details : 'Crystal Ducks & Turtle Figurines',
+        startPrice : '240$'
+    },
+
+    {
+        id: 14,
+        item : 'Pocket Watch Hunter Case',
+        image: 'http://localhost:3000/images/pocket-watch.jpg',
+        category : 'Jewelry',
+        status : 'Open',
+        startTime : 'October 10, 2021 5:00 PM',
+        endTime : 'November 25, 2021 3:00 PM',
+        details : 'Early 20th Century pocket watch; Original, 1.5" diameter',
+        startPrice : '150$'
+    },
+
+    {
+        id: 15,
+        item : 'Autographed Warner Bros Sericel',
+        image: 'http://localhost:3000/images/autographed-sericel.jpg',
+        category : 'Arts',
+        status : 'Closed',
+        startTime : 'October 31, 2021 8:00 AM',
+        endTime : 'November 19, 2021 3:00 PM',
+        details : 'Autographed Warner Bros Sylvester Hippetys Mummy sericel',
+        startPrice : '142$'
+    },
+
+    {
+        id: 16,
+        item : 'Bronze Bust',
+        image: 'http://localhost:3000/images/bronze-bust.jpg',
+        category : 'Crafts',
+        status : 'Open',
+        startTime : 'October 10, 2021 4:00 AM',
+        endTime : 'November 12, 2021 12:00 PM',
+        details : 'A vintage limited edition bronze bust by Gerson Frank',
+        startPrice : '500$'
     }
 ]
 
